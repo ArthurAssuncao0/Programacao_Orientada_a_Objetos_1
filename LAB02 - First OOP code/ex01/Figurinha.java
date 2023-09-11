be402527bfff56090079c562264f8c529890ed71
ArthurAssuncao0 <atumassuncao100204@gmail.com>
2023-09-11 18:07:23 -0300

    Lab02.1 - Att.

--- LAB02 - First OOP code/ex01/Figurinha.java
+++ LAB02 - First OOP code/ex01/Figurinha.java
@@ -1,27 +1,9 @@
 public class Figurinha{
-    private String nome;
-    private String dataNas;
-    private double peso;
-    private double altura;
-    private String pos;
-    private String pais;
+    public String nome;
+    public String dataNas;
+    public double peso;
+    public double altura;
+    public String pos;
+    public String pais;
 
-    public Figurinha(String nome, String dataNas, double altura, double peso, String pos, String pais) {
-        this.nome = nome;
-        this.dataNas = dataNas;
-        this.altura = altura;
-        this.peso = peso;
-        this.pos = pos;
-        this.pais = pais;
-    }
-
-    public void mostrarInformacoes() {
-        System.out.println("Nome do Jogador: " + nome);
-        System.out.println("Data de Nascimento: " + dataNas);
-        System.out.println("Altura: " + altura + " cm");
-        System.out.println("Peso: " + peso + " kg");
-        System.out.println("Posição: " + pos);
-        System.out.println("País: " + pais);
-        System.out.println();
-    }
 }
--- LAB02 - First OOP code/ex01/Main.java
+++ LAB02 - First OOP code/ex01/Main.java
@@ -2,31 +2,83 @@ import java.util.Scanner;
 
 public class Main{
    public static void main(String args[]){
-        Scanner x = new Scanner(System.in);
+       Scanner x = new Scanner(System.in);
 
-        Figurinha[] jogador = new Figurinha[3];
+        Figurinha jogador1 = new Figurinha();
+        Figurinha jogador2 = new Figurinha();
+        Figurinha jogador3 = new Figurinha();
 
-        for(int i = 0; i < 3; i++){
-            System.out.println("Digite o nome do jogador " + (i + 1) + ": ");
-            String nome = x.nextLine();
-            System.out.println("Digite a data de nascimento do jogador " + (i + 1) + ": ");
-            String dataNas = x.nextLine();
-            System.out.println("Digite o peso do jogador " + (i + 1) + ": ");
-            double peso = x.nextDouble();
-            System.out.println("Digite a altura do jogador " + (i + 1) + ": ");
-            double altura = x.nextDouble();
-            System.out.println("Digite a posição que o jogador " + (i + 1) + " atua: ");
-            String pos = x.nextLine();
-            System.out.println("Digite o país de nacionalidade do jogador " + (i + 1) + ": ");
-            String pais = x.nextLine();
+        System.out.println("Digite o nome do jogador 1: ");
+        jogador1.nome = x.nextLine();
+        System.out.println("Digite a data de nascimento do jogador 1: ");
+        jogador1.dataNas = x.nextLine();
+        System.out.println("Digite o peso do jogador 1: ");
+        jogador1.peso = x.nextDouble();
+        System.out.println("Digite a altura do jogador 1: ");
+        jogador1.altura = x.nextDouble();
+        System.out.println("Digite a posição que o jogador 1 atua: ");
+        x.nextLine(); // Limpeza do buffer de entrada
+        jogador1.pos = x.nextLine();
+        System.out.println("Digite o país de nacionalidade do jogador 1: ");
+        jogador1.pais = x.nextLine();
 
-            jogador[i] = new Figurinha(nome, dataNas, peso, altura, pos, pais);
-        }
+        System.out.println("Digite o nome do jogador 2: ");
+        jogador2.nome = x.nextLine();
+        System.out.println("Digite a data de nascimento do jogador 2: ");
+        jogador2.dataNas = x.nextLine();
+        System.out.println("Digite o peso do jogador 2: ");
+        jogador2.peso = x.nextDouble();
+        System.out.println("Digite a altura do jogador 2: ");
+        jogador2.altura = x.nextDouble();
+        System.out.println("Digite a posição que o jogador 2 atua: ");
+        x.nextLine(); // Limpeza do buffer de entrada
+        jogador2.pos = x.nextLine();
+        System.out.println("Digite o país de nacionalidade do jogador 2: ");
+        jogador2.pais = x.nextLine();
 
-       System.out.println("\nInformações das Figurinhas:");
-       for (int i = 0; i < 3; i++) {
-           jogador[i].mostrarInformacoes();
-       }
+        System.out.println("Digite o nome do jogador 3: ");
+        jogador3.nome = x.nextLine();
+        System.out.println("Digite a data de nascimento do jogador 3: ");
+        jogador3.dataNas = x.nextLine();
+        System.out.println("Digite o peso do jogador 3: ");
+        jogador3.peso = x.nextDouble();
+        System.out.println("Digite a altura do jogador 3: ");
+        jogador3.altura = x.nextDouble();
+        System.out.println("Digite a posição que o jogador 3 atua: ");
+        x.nextLine(); // Limpeza do buffer de entrada
+        jogador3.pos = x.nextLine();
+        System.out.println("Digite o país de nacionalidade do jogador 3: ");
+        jogador3.pais = x.nextLine();
+
+
+        System.out.println("\nInformações das Figurinhas:\n");
+
+        System.out.println("Jogador 1: \n");
+        System.out.println("Nome do Jogador: " + jogador1.nome);
+        System.out.println("Data de Nascimento: " + jogador1.dataNas);
+        System.out.println("Altura: " + jogador1.altura + " cm");
+        System.out.println("Peso: " + jogador1.peso + " kg");
+        System.out.println("Posição: " + jogador1.pos);
+        System.out.println("País: " + jogador1.pais);
+        System.out.println();
+
+        System.out.println("Jogador 2: \n");
+        System.out.println("Nome do Jogador: " + jogador2.nome);
+        System.out.println("Data de Nascimento: " + jogador2.dataNas);
+        System.out.println("Altura: " + jogador2.altura + " cm");
+        System.out.println("Peso: " + jogador2.peso + " kg");
+        System.out.println("Posição: " + jogador2.pos);
+        System.out.println("País: " + jogador2.pais);
+        System.out.println();
+
+        System.out.println("Jogador 3: \n");
+        System.out.println("Nome do Jogador: " + jogador3.nome);
+        System.out.println("Data de Nascimento: " + jogador3.dataNas);
+        System.out.println("Altura: " + jogador3.altura + " cm");
+        System.out.println("Peso: " + jogador3.peso + " kg");
+        System.out.println("Posição: " + jogador3.pos);
+        System.out.println("País: " + jogador3.pais);
+        System.out.println();
 
     }
 }
