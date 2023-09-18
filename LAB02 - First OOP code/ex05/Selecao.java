177d4e7e979e2d10c8cdab609d5e6f7a68126183
ArthurAssuncao0 <atumassuncao100204@gmail.com>
2023-09-18 16:06:36 -0300

    Lab02.5 - Att.

--- LAB02 - First OOP code/ex05/Selecao.java
+++ LAB02 - First OOP code/ex05/Selecao.java
@@ -15,32 +15,9 @@ public class Selecao{
         this.qtdTitulos = qtdTitulos;
     }
 
-    Figurinha[] jogador = new Figurinha[11];
-
-    public void criaJogador(String nome, String dataNas, double peso, double altura, String pos, String pais) {
-        for (int i = 0; i < 11; i++) {
-            jogador[i] = new Figurinha();
-
-            jogador[i].setNome(nome);
-            jogador[i].setDataNas(dataNas);
-            jogador[i].setPeso(peso);
-            jogador[i].setAltura(altura);
-            jogador[i].setPos(pos);
-            jogador[i].setPais(pais);
-        }
-    }
     public void mostraSelecao () {
         System.out.println("Seleção: " + nomeS);
         System.out.println("Técnico: " + tecnico);
         System.out.println("Quantidade de títulos: " + qtdTitulos);
-
-        for(int i = 0; i < 11; i++){
-            System.out.println(jogador[i].getNome());
-            System.out.println(jogador[i].getDataNas());
-            System.out.println(jogador[i].getPeso());
-            System.out.println(jogador[i].getAltura());
-            System.out.println(jogador[i].getPos());
-            System.out.println(jogador[i].getPos());
-        }
     }
 }
