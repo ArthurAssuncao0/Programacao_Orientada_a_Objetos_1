6de99392d1b011765d050ae6987564d4fd2d409a
ArthurAssuncao0 <atumassuncao100204@gmail.com>
2023-10-30 19:17:39 -0300

    Lab06.2 - Att.

--- LAB06 - Abstract classes/ex02/ContaComum.java
+++ LAB06 - Abstract classes/ex02/ContaComum.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class ContaComum {
+public class ContaComum extends ContaCorrente {
+
+    private float limite;
+    public ContaComum(float val, int num, int pwd) {
+        super(val, num, pwd);
+        limite = 0;
+    }
+
+    public void debitaValor(float val) {
+        if (val <= 0)
+            return;
+        if (val <= saldo) {
+            saldo -= val;
+        }
+    }
 }
