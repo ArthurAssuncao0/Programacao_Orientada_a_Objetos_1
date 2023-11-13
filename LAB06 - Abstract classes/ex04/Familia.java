6e4e04ac5de83b16429f969ad72592ca94886c7e
ArthurAssuncao0 <atumassuncao100204@gmail.com>
2023-11-13 17:41:18 -0300

    Lab06.4 - Att.

--- LAB06 - Abstract classes/ex04/Familia.java
+++ LAB06 - Abstract classes/ex04/Familia.java
@@ -1,5 +1,5 @@
 public class Familia extends Contato{
-    private String parentesco;
+    public String parentesco;
 
     public Familia(String apelido, String nome, String email, String aniversario, String parentesco){
         super(apelido, nome, email, aniversario);
